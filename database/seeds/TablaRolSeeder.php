c0ddd6b811b6b7fc8dc271b37596fef00742361d
Jack Chang <jackchangvera@gmail.com>
2020-04-06 01:29:19 -0500

    Trabajo con Seeders y Model Factory

---
+++ database/seeds/TablaRolSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class TablaRolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Roles definidos básicos
+        $rols = [
+            'administrador',
+            'editor',
+            'supervisor'
+        ];
+
+        foreach($rols as $key => $value) {
+            DB::table('rol')->insert([
+                'nombre' => $value,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+        }
+    }
+}
