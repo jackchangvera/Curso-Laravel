2b397f189318295119066ef905cf756bd4c2cf13
Jack Chang <jackchangvera@gmail.com>
2020-04-06 19:54:39 -0500

    implementacion del codigo AdminLTE a Blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'InicioController@index');
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +23,4 @@ Route::get('/', function () {
 Route::get('foo', function () {
     return 'Hello World';
 });
+*/
